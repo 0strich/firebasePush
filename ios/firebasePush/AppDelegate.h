119f12d7b19f23ab3b6aa877b3232e96df9577a2
김규동 <rich@daiblab.com>
2021-05-10 17:36:11 +0900

    [추가] iOS 알림 모듈 설치 및 설정 파일 수정

--- ios/firebasePush/AppDelegate.h
+++ ios/firebasePush/AppDelegate.h
@@ -1,5 +1,6 @@
 #import <React/RCTBridgeDelegate.h>
 #import <UIKit/UIKit.h>
+#import <UserNotifications/UNUserNotificationCenter.h> // @react-native-community/push-notification-ios
 
 @interface AppDelegate : UIResponder <UIApplicationDelegate, RCTBridgeDelegate>
 
